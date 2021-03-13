9f6bc49e23211215de07abd82cbc71b7322d5887
Jorge Eduardo <corpjorge@hotmail.com>
2021-03-13 13:34:28 -0500

    Reset

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import IconBadge from './IconBadge';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<IconBadge />, document.getElementById('root'));
-serviceWorker.unregister();
